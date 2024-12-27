3645a66fb2bb5684deb7bbc6e855dd95410818f5
Soumabha Roy <soumabha@itobuz.com>
2024-12-27 11:55:14 +0530

    image uploading

--- e2e/src/spec/media/upload-file.spec.ts
+++ e2e/src/spec/media/upload-file.spec.ts
@@ -7,7 +7,9 @@ import { CREATE_WORKSPACE_MUTATION } from '../../graphql/create-workspace-mutati
 import { faker } from '@faker-js/faker';
 import FormData from 'form-data';
 import fs from "fs";
-import { UploadFile } from "../../../../e2e/interface/upload-file"
+import path from 'path';
+import { UploadFile } from 'e2e/interface/upload-media-interface';
+
 
 const userArrays = [UserType.ADMIN, UserType.SUPER_ADMIN, UserType.USER];
 userArrays.forEach((userTypeRole) => {
@@ -54,9 +56,11 @@ userArrays.forEach((userTypeRole) => {
 
     test('Upload media', async () => {
       const formData = new FormData();
-      formData.append('file', fs.createReadStream('/src/lib/IMG_2060.jpeg'));
-    
+      const imagePath = path.join(process.cwd(), "src/images/IMG_2060.jpeg");
+      
       formData.append('description', 'Example description for the media');
+      formData.append('file', fs.createReadStream(imagePath));
+      
       const uploadFileResponse: UploadFile = await api.post(`${appEnv.API_BASE_URL}/media/upload`, formData, {
         headers: {
           ...formData.getHeaders(),
@@ -67,25 +71,8 @@ userArrays.forEach((userTypeRole) => {
       expect(uploadFileResponse.status).toBe(201);
       expect(uploadFileResponse.data).toHaveProperty('fileUrl');
     });
+    
   });
 });
 
-/*
-test("Get user profile details", async () => {
-      const headers = {
-        Authorization: `jwt ${loginToken}`,
-      };
-      const getProfileResponse: GetProfileResponse = await api.get(
-        "/api/user/profile",
-        {
-          headers,
-        }
-      );8
-      expect(getProfileResponse.message).toBe("User profile details");
-      if (userType === "ADMIN")
-        expect(getProfileResponse.data.email).toBe(appEnv.ADMIN_EMAIL);
-      else expect(getProfileResponse.data.email).toBe(appEnv.USER_EMAIL);
 
-      imageUrl = getProfileResponse.data.avatar;
-    });
-*/
