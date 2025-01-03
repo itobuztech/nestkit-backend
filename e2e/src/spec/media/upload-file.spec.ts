4b8dc5012605cd1e9f030cbb68c12d76a47cb4a2
Soumabha Roy <soumabha@itobuz.com>
2025-01-03 18:07:49 +0530

    get file done

--- e2e/src/spec/media/upload-file.spec.ts
+++ e2e/src/spec/media/upload-file.spec.ts
@@ -1,17 +1,25 @@
 import { appEnv } from '../../lib/app-env';
 import { PrismaClient, User, UserType } from '@prisma/client';
 import { GraphQlApi } from '../../lib/graphql-api';
-import { CreateWorkspaceMutation, CreateWorkspaceMutationVariables, FileQuery, FileQueryVariables, LoginQuery } from '../../gql/graphql';
+import {
+  CreateWorkspaceMutation,
+  CreateWorkspaceMutationVariables,
+  FileQuery,
+  FileQueryVariables,
+  LoginQuery,
+  QueryQuery,
+  QueryQueryVariables,
+} from '../../gql/graphql';
 import { CREATE_WORKSPACE_MUTATION } from '../../graphql/create-workspace-mutation.gql';
 import { faker } from '@faker-js/faker';
 import FormData from 'form-data';
-import fs from "fs";
+import fs from 'fs';
 import path from 'path';
 import { UploadFile } from '../../../interface/upload-media-interface';
 import axios from 'axios';
 import { ApolloQueryResult } from '@apollo/client';
 import { FILE_LIST_QUERY } from '../../graphql/list-file-query.gql';
-
+import { GET_FILE_QUERY } from '../../graphql/get-file-query.gql';
 
 const userArrays = [UserType.SUPER_ADMIN];
 userArrays.forEach((userTypeRole) => {
@@ -21,6 +29,7 @@ userArrays.forEach((userTypeRole) => {
     let workspaceId: string | undefined;
     const api = new GraphQlApi();
     let loginResponse: ApolloQueryResult<LoginQuery>;
+    let fileId: any;
 
     test(`Login as a ${userTypeRole.toUpperCase()}`, async () => {
       const dbClient = new PrismaClient();
@@ -59,43 +68,64 @@ userArrays.forEach((userTypeRole) => {
 
     test('Upload media', async () => {
       const formData = new FormData();
-      const imagePath = path.join(process.cwd(), "src/images/IMG_2060.jpeg");
+      const imagePath = path.join(process.cwd(), 'src/images/IMG_2060.jpeg');
       formData.append('file', fs.createReadStream(imagePath));
-      
-     try {
-      const uploadFileResponse = await axios.post(`${appEnv.API_BASE_URL}/media/upload`, formData, {
-        headers: {
-          ...formData.getHeaders(),
-          current_workspace_id: workspaceId,
-          Authorization: `Bearer ${loginResponse.data.login.token}`
-        },
-      });
-      const fileResponse: UploadFile = uploadFileResponse.data;
-      console.log(fileResponse);    
-      expect(fileResponse.id).toBeDefined();
-     } catch (err) {
-      console.error(err);
-     }
+
+      try {
+        const uploadFileResponse = await axios.post(
+          `${appEnv.API_BASE_URL}/media/upload`,
+          formData,
+          {
+            headers: {
+              ...formData.getHeaders(),
+              current_workspace_id: workspaceId,
+              Authorization: `Bearer ${loginResponse.data.login.token}`,
+            },
+          },
+        );
+        const fileResponse: UploadFile = uploadFileResponse.data;
+        console.log(fileResponse);
+        fileId = fileResponse.id;
+        console.log(fileId);
+        expect(fileResponse.id).toBeDefined();
+      } catch (err) {
+        console.error(err);
+      }
     });
 
     test('List of media file', async () => {
-              const fileList = await api.graphql.query<
-              FileQuery,
-              FileQueryVariables
-              >({
-                query: FILE_LIST_QUERY,
-    
-                context: {
-                  headers: {
-                    current_workspace_id: workspaceId,
-                    Authorization: `Bearer ${loginResponse.data.login.token}`
-                  },
-                },
-              });
-      
-              expect(fileList.data.listMedia.file).toBeDefined();
-              expect(fileList.data.listMedia.file[0].id).not.toHaveLength(0);
-          });
+      const fileList = await api.graphql.query<FileQuery, FileQueryVariables>({
+        query: FILE_LIST_QUERY,
+
+        context: {
+          headers: {
+            current_workspace_id: workspaceId,
+            Authorization: `Bearer ${loginResponse.data.login.token}`,
+          },
+        },
+      });
+
+      expect(fileList.data.listMedia.file).toBeDefined();
+      expect(fileList.data.listMedia.file[0].id).not.toHaveLength(0);
     });
-    
+
+    test('Get media file', async () => {
+      const getFile = await api.graphql.query<QueryQuery, QueryQueryVariables>({
+        query: GET_FILE_QUERY,
+        variables: {
+          getFileInput: {
+            id: fileId,
+          },
+        },
+        context: {
+          headers: {
+            current_workspace_id: workspaceId,
+            Authorization: `Bearer ${loginResponse.data.login.token}`,
+          },
+        },
+      });
+
+      expect(getFile.data.getFile?.id).toBeDefined();
+    });
   });
+});
