2f7192f31f03e8085e7f1f32d7a3659b3f97fb1f
Prosenjit Manna <prosenjit@itobuz.com>
2025-01-17 12:54:55 +0530

    feat: update assigned user role input

--- src/roles/assign-role-users/assign-role-user-input.dto.ts
+++ src/roles/assign-role-users/assign-role-user-input.dto.ts
@@ -1,11 +1,28 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { BaseListInput } from 'src/shared/base-list/base-list-input.dto';
+import { Field, InputType, Int, registerEnumType } from '@nestjs/graphql';
+
+export enum AssignRoleUserOrder {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+registerEnumType(AssignRoleUserOrder, {
+  name: 'Order',
+});
 
 @InputType()
-export class AssignRoleUserInput extends BaseListInput {
+export class AssignRoleUserInput {
   @Field(() => String)
   roleId: string;
 
   @Field(() => String, { nullable: true })
   search?: string;
+
+   @Field(() => Int, { nullable: true })
+    page?: number;
+  
+    @Field(() => Int, { nullable: true })
+    pageSize?: number;
+  
+    @Field(() => AssignRoleUserOrder, { nullable: true, defaultValue: AssignRoleUserOrder.DESC })
+    orderBy?: AssignRoleUserOrder;
 }
