0ac19281f279e07528a2d24c529f55a51f5d27b8
Prosenjit Manna <prosenjit@itobuz.com>
2025-01-20 19:33:09 +0530

    fix: delete assigned role

--- src/roles/unassign-role/unassign-role.service.ts
+++ src/roles/unassign-role/unassign-role.service.ts
@@ -1,5 +1,6 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { HttpStatus, SetMetadata, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
 
 import { UnAssignRoleResponse } from './unassign-role-response.dto';
 import { UnAssignRoleInput } from './unassign-role-input.dto';
@@ -8,6 +9,8 @@ import { CreateAppError } from 'src/shared/create-error/create-error';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { RoleGuard } from 'src/auth/role.guard';
 import { PrivilegeGroup, PrivilegeName } from '@prisma/client';
+import { WorkspaceMemberShipGuard } from 'src/auth/workspace-membership.guard';
+import { MemberShipValidationType } from 'src/auth/membership-validation-type.enum';
 
 @Resolver()
 @UseGuards(JwtAuthGuard)
@@ -18,8 +21,17 @@ export class UnAssignRoleService {
   @UseGuards(RoleGuard)
   @SetMetadata('privilegeGroup', PrivilegeGroup.ROLE)
   @SetMetadata('privilegeName', PrivilegeName.UPDATE)
+
+
+  @UseGuards(WorkspaceMemberShipGuard)
+  @SetMetadata(
+    'memberShipValidationType',
+    MemberShipValidationType.MEMBERSHIP_VALIDITY,
+  )
+    
   async unAssignRole(
     @Args('unAssignRoleInput') assignRoleInput: UnAssignRoleInput,
+    @Context('req') req: Request,
   ): Promise<UnAssignRoleResponse> {
 
 
@@ -27,6 +39,7 @@ export class UnAssignRoleService {
       where: {
         userId: assignRoleInput.userId,
         roleId: assignRoleInput.roleId,
+        workspaceId: req.currentWorkspaceId,
       },
     });
 
